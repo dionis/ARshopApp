1bc05f7ed28f9b136c420491e43b37cf78bcdc09
YilbertBB <132036466+YilbertBB@users.noreply.github.com>
2024-07-07 23:49:15 -0400

    Delete Backend directory

--- Backend/producto_app/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Categoria, Producto, Entidad
-from django.db import models
-
-# Register your models here.
-admin.site.register(Categoria)
-admin.site.register(Entidad)
-admin.site.register(Producto)
